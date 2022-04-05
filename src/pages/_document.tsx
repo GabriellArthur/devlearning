e2b935774a5e43ed18dec4220267bb78cce0c6d3
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-04-05 09:54:08 -0300

    init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <header>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </header>
+        <body>
+          
+        </body>
+      </html>
+    )
+  }
+}
