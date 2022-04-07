69af150b861b0dd073e0f7f47cc6aba95c571112
luiz-cazarin <57879201+luiz-cazarin@users.noreply.github.com>
2022-04-07 10:19:17 -0300

    tela inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,23 @@
 import styles from './styles.module.scss'
-import Link from 'next/link';
+import {ActiveLink} from '../ActiveLink';
+
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        <img src='/images/devLearning.svg' alt="devLearning" />
         <nav>
+          <ActiveLink href='/' activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href='/aulas' activeClassName={styles.active}>
+            <a>Aulas</a>
+          </ActiveLink>
+          <ActiveLink href='/Login' activeClassName={styles.active}>
+            <a>Login</a>
+          </ActiveLink>
+          
         </nav>
       </div>
     </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Head from 'next/head';
 import Link from 'next/link';
 import styles from './home.module.scss';
 
+import Lottie from 'react-lottie';
+
+import animation from '../../public/lottie/404.json';
+
 
 export default function Home() {
    return (
@@ -11,8 +15,16 @@ export default function Home() {
          </Head>
 
          <main className={styles.contentContainer}>
-            <div>
-               <p>a</p>
+            <section className={styles.hero}>
+               <h1>Seja um desenvolvedor Web</h1>
+               <span>Aprenda o basico para criar o seu primeiro site</span>
+               <p>Este site foi construido por alunos de ciencias da computacao da universidade catolica, com ituito de ajudar outros alunos na sua carreira, aqui veremos o basico em logica de programacao, html, css e java script, alem de um guia de carreira para que voce possa ir alem e possa crescer na sua carreira</p>
+               
+               <a href="/Aulas"><button className={styles.button}>Inicar Curso</button></a>
+               <a href="/about"><button className={styles.buttonActive}>Saiba mais</button></a>
+            </section>
+            <div className={styles.lottie}>
+            <img src='/images/computador-hello-world.svg' alt="devLearning" />
             </div>
          </main>
       </>
