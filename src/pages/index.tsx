5f96c45eae38755817f1148c2d9c12e70912056c
LuizClaudio <cazarin.junior@gmail.com>
2022-09-12 16:35:14 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,8 @@ export default function Home() {
                <section className={styles.hero}>
                   <h1>Seja um desenvolvedor Web</h1>
                   <span>Aprenda o basico para criar o seu primeiro site</span>
-                  <p>Este site foi construido por alunos de ciencias da computacao da universidade catolica, com ituito de ajudar outros alunos na sua carreira, aqui veremos o basico em logica de programacao, html, css e java script, alem de um guia de carreira para que voce possa ir alem e possa crescer na sua carreira</p>
-                  
-                  <a href="/web/class"><button className={styles.button}>Inicar Curso</button></a>
+                  <p>Este site foi construido por alunos de ciencias da computação da Universidade Católica, com o intuito de ajudar outros alunos na sua carreira, aqui veremos o basico em logica de programação, HTML, CSS e JavaScript, alem de um guia de carreira para que voce possa ir alem e possa crescer na sua carreira</p>
+                  <a href="/web/class"><button className={styles.button}>Iniciar Curso</button></a>
                   <a href="/web/about"><button className={styles.buttonActive}>Saiba mais</button></a>
                </section>
                <aside className={styles.lottie}>
